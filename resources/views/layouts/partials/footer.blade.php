199cafb07032333a3687e0b1c81350453885a91b
arturo <arturomazzoni@gmail.com>
2021-04-08 23:52:54 +0200

    collegamento sass + layouts
    Header e footer ok

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,64 @@
+<footer>
+  <div class="container">
+    <div class="footer-top">
+      <ul>
+        <li>Informazioni</li>
+        <li>Come funziona</li>
+        <li>Opportinità di lavoro</li>
+        <li>Newsroom</li>
+      </ul>
+      <ul>
+        <li>Community</li>
+        <li>AirBnb.org</li>
+        <li>Accessibilità</li>
+        <li>Invitare un ospite</li>
+        <li>Diversità e appartenenza</li>
+      </ul>
+      <ul>
+        <li>Ospita</li>
+        <li>Diventa un host</li>
+        <li>Ospitare responsabilmente</li>
+        <li>Centro Risorse</li>
+      </ul>
+      <ul>
+        <li>Assistenza</li>
+        <li>La nostra risposta all'emergenza COVID-19</li>
+        <li>Centro assistenza</li>
+        <li>Opzioni di cancellazione</li>
+        <li>Affidabilità e sicurezza</li>
+        <li>Contatti</li>
+      </ul>
+    </div>
+    <!-- fine footer-top -->
+    <div class="footer-bottom">
+      <ul>
+        <li>&#xa9;2021 AirBnb, Inc. all rights reserved</li>
+        <li><a href="">Privacy</a></li>
+        <li><a href="">Termini</a></li>
+        <li><a href="">Mappa del sito</a></li>
+        <li><a href="">Dettagli dell'azienda</a></li>
+      </ul>
+      <ul>
+        <li>
+          <i class="fas fa-globe"></i>
+          <a href="">Italiano(IT)</a>
+        </li>
+        <li>
+          &#8364; 
+          <a href="">EUR</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+        <button type="button" class="btn-social"><i class="icon-size fab fa-facebook-f"></i></button>
+        </li>
+        <li>
+          <button type="button" class="btn-social"><i class="icon-size fab fa-twitter"></i></button>
+        </li>
+        <li>
+          <button type="button" class="btn-social"><i class="icon-size fab fa-instagram"></i></button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</footer>
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,103 @@
+<header>
+
+
+    {{-- @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/admin') }}">Area amministrativa dell'host</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}">Register</a>
+                @endif
+            @endauth
+        </div>
+    @endif --}}
+
+  <div class="container">
+    <div class="header-bar">
+      <div class="logo"><a href="#"><img src="img/logo.png" style="width:200px;" alt="Logo Airbnb"></a></div>
+      <div class="header-menu">
+        <ul>
+          <li><a href="#" >Alloggi</a></li>
+          <!-- <li><a href="#" >Places to stay</a></li> -->
+          <li><a href="#" >Esperienze</a></li>
+          <!-- <li><a href="#" >Experiences</a></li> -->
+          <li><a href="#" >Esperienze online</a></li>
+          <!-- <li><a href="#">Online Experiences</a></li> -->
+        </ul>
+      </div>
+      <div class="header-right">
+        <ul>
+          <li>
+            <i class="fas fa-globe"></i>
+          </li>
+          <li>
+            @if (Route::has('login'))
+              <div class="border-user">
+                @auth
+                  <a href="{{ url('/admin') }}">Area amministrativa dell'host</a>
+                @else
+                  <a href="{{ route('login') }}">Login</a>
+
+                  @if (Route::has('register'))
+                      <a href="{{ route('register') }}">Register</a>
+                  @endif
+                @endauth
+              </div>
+            @endif
+          </li>
+        </ul>
+      </div>
+
+    </div> <!--   fine .header-menu -->
+    <div class="header-form">
+      <form class="search">
+        <ul>
+          <li>
+            <div class="search-inputs">
+              <label for="location">
+                <div class="label">Dove</div>
+                <!-- <div class="label">Location</div> -->
+                <input name="location" id="location" placeholder="Dove vuoi andare?" />
+                <!-- <input name="location" id="location" placeholder="Where are you going?" /> -->
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="search-inputs">
+              <label for="check-in">
+                <div class="label">Check in</div>
+                <input name="check-in" id="check-in" placeholder="Aggiungi date" />
+                <!-- <input name="check-in" id="check-in" placeholder="Add dates" /> -->
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="search-inputs">
+              <label for="check-out">
+                <div class="label">Check out</div>
+                <input name="check-out" id="check-out" placeholder="Aggiungi date" />
+                <!-- <input name="check-out" id="check-out" placeholder="Add dates" /> -->
+              </label>
+            </div>
+          </li>
+          <li>
+            <div class="search-inputs">
+              <label for="guests">
+                <div class="label">Ospiti</div>
+                <!-- <div class="label">Guests</div> -->
+                <input name="guests" id="guests" placeholder="Aggiungi ospiti" />
+                <!-- <input name="guests" id="guests" placeholder="Add guests" /> -->
+              </label>
+            </div>
+            <button class="button-search">
+              <i class="fas fa-search fa-xs"></i>
+            </button>
+          </li>
+        </form>
+      </ul>
+    </div>
+  </div> <!--   fine container -->
+</header>
