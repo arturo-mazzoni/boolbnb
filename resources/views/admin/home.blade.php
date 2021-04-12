cca416452dad0e283ef1d9216a1062f21e31cd17
alessandro <finialessandro90@gmail.com>
2021-04-12 17:17:08 +0200

    dashboard admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,6 @@
-@extends('layouts.app')
+@extends('layouts.admin')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row dashboard-table">
-            @include('layouts.partials.dashboard') {{-- Dashboard Utente registrato --}}
-      <h1>admin home</h1>
-    </div>
-</div>
+
 
 @endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
+           
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+                    {{-- <a class="navbar-brand" href="{{ route('guest.posts.index' ) }}"> --}}
+                    Post
+                    </a>
+                
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav  ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle text-white" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Esci') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                    <a class="dropdown-item" href="{{  url('/admin') }}">Admin</a>
+
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            
+        </nav>
+
+        <main>
+        <div class="wrapp2">
+            <div class="left ">
+                <ul>
+                    <li>
+                        <a href="{{route('home')}}"><i class="fas fa-home"></i>DashBoard</a>
+                    </li>
+                    <li>
+                        <a href=""><i class="fas fa-users"></i>Account</a>
+                    </li>
+                    <li>
+                        {{-- <a href="{{route('post.index')}}"><i class="far fa-sticky-note"></i>Post</a> --}}
+                    </li>
+                    <li>
+                        <a href=""><i class="fas fa-tag"></i>Tags</a>
+                    </li>
+                
+                </ul>
+            </div>
+
+            <div class="right">
+                @yield('content')
+            </div>
+        
+        </div>
+            
+        </main>
+    </div>
+</body>
+</html>
