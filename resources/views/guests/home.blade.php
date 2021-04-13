574bc228d8cde725e3ff84a4139a2dc18a0d437a
Leandro <thesquallrock@gmail.com>
2021-04-13 22:59:26 +0200

    homepage layout bozza

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,7 +1,145 @@
 @extends('layouts.app')
 
 @section('content')
-questa è la vetrina
+<div class="my-jumbotron">
+    <div class="jumbo-image">
+        <img src="https://a0.muscache.com/im/pictures/166791ff-bc82-4b88-ba3d-49be1d462dce.jpg" alt="jumbotron">
+    </div>
+    <div class="jumbo-text">
+        <span>Grazie agli host puoi</span>
+    </div>
+</div>
+
+<section class="explore-home">
+    <h2>Esplora i dintorni</h2>
+    <div class="explore-list">
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+        <div class="explore-object">
+            <div class="explore-box"></div>
+            <div class="explore-text">
+                <div>Città</div>
+                <span>Distanza</span>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="houses-home">
+    <h2>Una casa ovunque nel mondo</h2>
+    <div class="houses-list">
+        <div class="houses-object">
+            <div class="houses-box"></div>
+            <div class="houses-text">
+                <div>Testo</div> 
+            </div>
+        </div>
+        <div class="houses-object">
+            <div class="houses-box"></div>
+            <div class="houses-text">
+                <div>Testo</div> 
+            </div>
+        </div>
+        <div class="houses-object">
+            <div class="houses-box"></div>
+            <div class="houses-text">
+                <div>Testo</div> 
+            </div>
+        </div>
+        <div class="houses-object">
+            <div class="houses-box"></div>
+            <div class="houses-text">
+                <div>Testo</div> 
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="become-host-home">
+    <img src="https://a0.muscache.com/im/pictures/48893cdc-221e-4eaf-b624-8956600550db.jpg" alt="become-host">
+    <div class="become-host-interact">
+        <div>
+            <h2>Diventa host</h2>
+            <p>Condividi il tuo spazio per guadagnare qualcosa in più e scoprire nuove opportunità.</p>
+            <a class="my-btn-home" href="">Scopri di più</a>
+        </div>
+    </div>
+</section>
+
+<section class="experiences-home">
+    <h2>Scopri le esperienze</h2>
+    <p>Attività uniche con esperti del luogo, di persona oppure online.</p>
+    <div class="experiences-list">
+        <div class="experiences-object">
+            <div class="experiences-box"></div>
+            <div class="experiences-text">
+                <div>Testo</div>
+                <span>Testo lungo</span>
+            </div>
+        </div>
+        <div class="experiences-object">
+            <div class="experiences-box"></div>
+            <div class="experiences-text">
+                <div>Testo</div>
+                <span>Testo lungo</span>
+            </div>
+        </div>
+        <div class="experiences-object">
+            <div class="experiences-box"></div>
+            <div class="experiences-text">
+                <div>Testo</div>
+                <span>Testo lungo</span>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 
 
