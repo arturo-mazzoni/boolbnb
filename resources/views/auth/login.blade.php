43812aea70faf7fdf0d7a7e6ba2ddf0e8e532622
alessandro <finialessandro90@gmail.com>
2021-04-19 17:33:38 +0200

    margin bottom + colori aloha button login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" style="margin-top: 250px">
+<div class="container" style="margin-top: 250px; margin-bottom: 150px">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn" style="background-color: #004C99; color: #F5B964">
                                     {{ __('Login') }}
                                 </button>
 
